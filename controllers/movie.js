a7989635632666aa64cdc81da11b67e3fc11d32e
Cameron  Morgan <cm09516@georgiasouthern.edu>
2019-10-21 14:18:55 -0400

    initial mvc

---
+++ controllers/movie.js
@@ -0,0 +1,64 @@
+const express = require('express')
+
+const movieApi = require('../models/movie.js')
+
+const movieRouter = express.Router()
+
+movieRouter.get('/movie/new', (req, res) => {
+  res.render('movie/createMovieForm')
+})
+
+// movieRouter.get('/movie/new/:directorId', (req, res) => {
+//   res.render('movie/createMovieForm', {directorId: req.params.directorId})
+// })
+
+movieRouter.get('/movie/edit/:id', (req, res) => {
+  movieApi.getOneMovie(req.params.id)
+    .then((singleMovie) => {
+      res.render('movie/editMovieForm', singleMovie)
+    })
+})
+
+// getAll
+movieRouter.get('/movie', (req, res) => {
+  movieApi.getAllMovies()
+    .then((allMovies) => {
+      //res.json(allMovies)
+      res.render('movie/allMovies', { allMovies })
+    })
+})
+// getOne
+movieRouter.get('/movie/:id', (req, res) => {
+  movieApi.getOneMovie(req.params.id)
+    .then((singleMovie) => {
+      //res.json(singleMovie)
+      res.render('movie/singleMovie', singleMovie)
+    })
+})
+// update
+movieRouter.put('/movie/:id', (req, res) => {
+  movieApi.updateMovie(req.params.id, req.body)
+    .then((updatedMovie) => {
+      //res.json(updatedMovie)
+      res.redirect(`/movie/${req.params.id}`)
+    })
+})
+// create
+movieRouter.post('/movie', (req, res) => {
+  movieApi.createMovie(req.body)
+    .then((createdMovie) => {
+      res.redirect("/movie")
+    })
+})
+// delete
+movieRouter.delete('/movie/:id', (req, res) => {
+  movieApi.deleteMovie(req.params.id)
+    .then((deletedMovie) => {
+      //res.json(deletedMovie)
+      res.redirect("/movie")
+    })
+})
+
+module.exports = {
+  movieRouter
+}
---
+++ models/movie.js
@@ -0,0 +1,51 @@
+const mongoose = require('./connection.js')
+
+const MovieSchema = new mongoose.Schema({
+  name: String,
+  releaseYear: Number,
+  genre: String,
+  director: String,
+  picture: String,
+
+})
+
+const MovieCollection = mongoose.model('Movie', MovieSchema)
+
+//getAll
+const getAllMovies = () => {
+  return MovieCollection.find({})
+}
+
+//get all Movies By Director Id
+// const getallMoviesByDirectorId = (directorId) => {
+//   return MovieCollection.find({directorId: directorId})
+// }
+
+//getOne
+const getOneMovie = (id) => {
+  return MovieCollection.findById(id)
+}
+
+//create
+const createMovie = (movieData) => {
+  return MovieCollection.create(movieData)
+}
+
+//update
+const updateMovie = (id, movieData) => {
+  return MovieCollection.updateOne({_id: id}, movieData)
+}
+
+//delete
+const deleteMovie = (id) => {
+  return MovieCollection.deleteOne({_id: id})
+}
+
+module.exports = {
+  getAllMovies,
+  //getallMoviesByDirectorId,
+  getOneMovie,
+  createMovie,
+  updateMovie,
+  deleteMovie
+}
